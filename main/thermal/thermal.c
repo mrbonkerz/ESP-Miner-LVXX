64dd05f6c2726b83f9e7dc50095a870535369afd
mrbonkerz <52251984+mrbonkerz@users.noreply.github.com>
2025-03-11 13:09:43 +0800

    combined LVXX together

--- main/thermal/thermal.c
+++ main/thermal/thermal.c
@@ -90,7 +90,6 @@ float Thermal_get_chip_temp(GlobalState * GLOBAL_STATE) {
         case DEVICE_GAMMATURBO:
             return EMC2103_get_external_temp();
         case DEVICE_LV07:
-            return TMP1075_read_temperature(0) + INTERNAL_OFFSET;
         case DEVICE_LV08:
             return TMP1075_read_temperature(0) + INTERNAL_OFFSET;
         default:
