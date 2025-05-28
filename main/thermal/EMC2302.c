3cbaca2b6a9a39f02bfa715c32da3595f7a30a92
mrbonkerz <52251984+mrbonkerz@users.noreply.github.com>
2025-05-28 10:53:26 +0800

    TCH/ESP-Miner-Zyber

--- main/thermal/EMC2302.c
+++ main/thermal/EMC2302.c
@@ -11,24 +11,20 @@
 #define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
 #define I2C_MASTER_TIMEOUT_MS 1000
 
-static const char *TAG = "EMC2302";
+static const char *TAG = "EMC2302.c";
 
 // run this first. sets up the config register
 static i2c_master_dev_handle_t emc2302_dev_handle;
 
-esp_err_t EMC2302_init() {
-    if (i2c_bitaxe_add_device(EMC2302_I2CADDR_DEFAULT, &emc2302_dev_handle, TAG) != ESP_OK) {
+esp_err_t EMC2302_init(void) {
+    if (i2c_bitaxe_add_device(EMC2302_I2CADDR_DEFAULT, &emc2302_dev_handle,TAG) != ESP_OK) {
         ESP_LOGE(TAG, "Failed to add device");
         return ESP_FAIL;
     }
 
-    //if (invertPolarity) {
-    ESP_ERROR_CHECK(i2c_bitaxe_register_write_byte(emc2302_dev_handle, EMC2302_PWM_POLARITY, 0b00011111));
-    //}
-
-    // Set fan range to 00: 500 RPM minimum, TACH count multiplier = 1
-    // fan config default before register write: 2B = 00101011
-    ESP_ERROR_CHECK(i2c_bitaxe_register_write_byte(emc2302_dev_handle, EMC2302_FAN1_CONFIG1, 0b00001011));
+    if (true) {
+        ESP_ERROR_CHECK(i2c_bitaxe_register_write_byte(emc2302_dev_handle, EMC2302_PWM_POLARITY, 0b00011111));
+    }
 
     return ESP_OK;
 }
@@ -54,17 +50,10 @@ uint16_t EMC2302_get_fan_speed(uint8_t devicenum)
     ESP_ERROR_CHECK(i2c_bitaxe_register_read(emc2302_dev_handle,TACH_LSB_REG, &tach_lsb, 1));
     ESP_ERROR_CHECK(i2c_bitaxe_register_read(emc2302_dev_handle,TACH_MSB_REG, &tach_msb, 1));
 
-    //ESP_LOGI(TAG, "Raw Fan Speed[%d] = %02X %02X", devicenum, tach_msb, tach_lsb);  // DEBUG
+    //ESP_LOGI(TAG, "Raw Fan Speed[%d] = %02X %02X", devicenum, tach_msb, tach_lsb);
     RPM = (tach_msb << 5) + ((tach_lsb >> 3) & 0x1F);
     RPM = EMC2302_FAN_RPM_NUMERATOR / RPM;
-    //ESP_LOGI(TAG, "Fan Speed[%d] = %d RPM", devicenum, RPM);                        // DEBUG
-
-    // DEBUG: get fan config
-    //uint8_t fan_conf;
-    //uint8_t FAN1_CONFIG1 = EMC2302_FAN1_CONFIG1 + (devicenum * 0x10);
-    //ESP_ERROR_CHECK(i2c_bitaxe_register_read(emc2302_dev_handle, FAN1_CONFIG1, &fan_conf, 1));
-    //ESP_LOGI(TAG, "Fan config[%d] = %02X", devicenum, fan_conf);
-    // DEBUG
+    //ESP_LOGI(TAG, "Fan Speed[%d] = %d RPM", devicenum, RPM);
 
     return RPM;
 }
@@ -80,4 +69,3 @@ uint8_t EMC2302_get_internal_temp(void)
     // We don't have temperature on this chip, so fake it
     return 0;
 }
-
